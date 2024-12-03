40fa0dd06f19fea775ebcd2335722f9ab89af889
bHannah <borsikaaa1@gmail.com>
2024-12-03 11:20:44 +0100

    api végpontok a kérdésekhez és a válaszokhoz

--- app/Http/Controllers/ExerciseController.php
+++ app/Http/Controllers/ExerciseController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Exercise;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class ExerciseController extends Controller
@@ -12,4 +13,18 @@ class ExerciseController extends Controller
         return Exercise::all();
     }
 
+    public function specExercise(string $exercise_id)
+    {
+        return DB::table('exercises as e')
+        ->select('mondat')
+        ->where('exercise_id','=', $exercise_id)
+        ->get();
+    }
+
+    public function exercisesWithAnswers()
+    {
+        return DB::table ('exercises as e')
+        ->join('answers as a', 'a.melyik_kerdes', 'e.exercise_id')
+        ->get();
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,14 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+//kérdésekre vonatkozó lekérések
+Route::get('spec-exercise-param/{exercise_id}', [ExerciseController::class, "specExercise"]);
+
+//válaszokra vonatkozó lekérések
 Route::get('answers-for-spec-exercise-param/{exercise_id}', [AnswerController::class, "answersForSpecExerciseParam"]);
 Route::get('answers-for-spec-exercise', [AnswerController::class, "answersForSpecExercise"]);
 Route::get('answers', [AnswerController::class, "allAnswers"]);
 Route::get('exercises', [ExerciseController::class, "allExercises"]);
+
+//kérdéseket együtt adja vissza a válaszokkal
+Route::get('exercises-with-answers/{exercise_id}',[ExerciseController::class, "exercisesWithAnswers"]);
